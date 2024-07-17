4381a6ad605e2fa94fbcbee7ee3f2d6bd67c92b3
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-07-17 08:20:15 +0000

    fix: docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,7 @@
 : ${ORIGIN:="http://localhost:3000"}
 : ${BACKEND_URL:="http://127.0.0.1"}
 
-exec ORIGIN=$ORIGIN BACKEND_URL=$BACKEND_URL node /riven/build
+export ORIGIN
+export BACKEND_URL
+
+exec node /riven/build
