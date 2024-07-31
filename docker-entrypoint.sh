3e3ff184ae164a7dbd189c2fe31f3ed75caae686
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-07-31 22:30:42 +0530

    fix: docker entrypoint and docs improvement (#40)
    * fix: docker compose and entrypoint
    
    * chore: docs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 # Export the ORIGIN and BACKEND_URL environment variables
 export ORIGIN=${ORIGIN}
 export BACKEND_URL=${BACKEND_URL}
+export DIALECT=${DIALECT}
+export DATABASE_URL=${DATABASE_URL}
 
 # Execute the command provided to the script
 exec "$@"
