c16c67ecc4f17c42240184a702613a7f8c10889e
Filip Trplan <filip@trplan.si>
2024-10-04 18:58:55 +0200

    feat: add backend proxy

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,7 @@
-import { env } from '$env/dynamic/public';
-const BACKEND_URL = env.PUBLIC_BACKEND_URL || 'http://127.0.0.1:8080';
 import { client } from './client/services.gen';
 
 client.setConfig({
-	baseUrl: BACKEND_URL
+	baseUrl: "/api"
 });
 
 client.interceptors.error.use((error) => {
