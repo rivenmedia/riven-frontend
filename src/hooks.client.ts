10f7a6d9796873359d872c955cb246785d67a256
Filip Trplan <filip@trplan.si>
2024-10-05 14:15:41 +0200

    fix: fix imports on hooks

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,10 +1,10 @@
-import { client } from './client/services.gen';
+import { client } from '$lib/client/services.gen';
 
 client.setConfig({
 	baseUrl: '/api'
 });
 
-client.interceptors.error.use((error) => {
+client.interceptors.error.use((error: unknown) => {
 	if (error && typeof error == 'object' && 'detail' in error && typeof error.detail == 'string') {
 		return error.detail;
 	}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ import { sequence } from '@sveltejs/kit/hooks';
 import { env } from '$env/dynamic/private';
 const BACKEND_URL = env.BACKEND_URL || 'http://127.0.0.1:8080';
 import { db } from '$lib/server/db';
-import { client, DefaultService } from './client/services.gen';
+import { client, DefaultService } from '$lib/client/services.gen';
 
 const setLocals: Handle = async ({ event, resolve }) => {
 	event.locals.BACKEND_URL = BACKEND_URL;
@@ -33,7 +33,7 @@ client.setConfig({
 	baseUrl: BACKEND_URL
 });
 
-client.interceptors.error.use((error) => {
+client.interceptors.error.use((error: unknown) => {
 	if (error && typeof error == 'object' && 'detail' in error && typeof error.detail == 'string') {
 		return error.detail;
 	}
