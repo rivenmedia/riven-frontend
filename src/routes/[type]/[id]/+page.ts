6dc38f83ca44b616e16c3cbe2f6348c6aca32ed3
Filip Trplan <filip@trplan.si>
2024-10-10 13:02:45 +0200

    fix: move the item page to frontend loading

--- src/routes/[type]/[id]/+page.ts
+++ src/routes/[type]/[id]/+page.ts
@@ -1,9 +1,23 @@
 import type { PageLoad } from './$types';
 import { getMovieDetails, getTVDetails } from '$lib/tmdb';
+import { ItemsService } from '$lib/client';
 
-export const load = (async ({ fetch, params, data }) => {
+export const load = (async ({ fetch, params }) => {
 	const type = params.type;
 	const id = Number(params.id);
+
+	const { data } = await ItemsService.getItem({
+		path: {
+			id: id
+		},
+		query: {
+			use_tmdb_id: true,
+		}
+	});
+
+	if (data) {
+		data.requested_at = new Date(data.requested_at as string);
+	}
 
 	async function getDetails(type: string, id: number) {
 		switch (type) {
@@ -29,6 +43,7 @@ export const load = (async ({ fetch, params, data }) => {
 		details: await getDetails(type, id),
 		mediaType: type,
 		mediaID: id,
-		...data
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		db: data as any
 	};
 }) satisfies PageLoad;
