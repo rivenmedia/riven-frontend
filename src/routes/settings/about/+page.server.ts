b7bd4605c2b705c90180733c68a61218bf1b215a
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-10-18 19:12:09 +0000

    fix: frontend version showing unknown

---
+++ src/routes/settings/about/+page.server.ts
@@ -0,0 +1,23 @@
+import type { PageServerLoad } from './$types';
+import fs from 'fs/promises';
+import path from 'path';
+import { dev } from '$app/environment';
+
+export const prerender = true;
+
+export const load = (async () => {
+	let versionFilePath: string = '/riven/version.txt';
+	if (dev) {
+		versionFilePath = './version.txt';
+	}
+	let frontendVersion = 'Unknown';
+	try {
+		frontendVersion = (await fs.readFile(path.resolve(versionFilePath), 'utf-8')).trim();
+	} catch (err) {
+		console.error('Error reading frontend version file:', err);
+	}
+
+	return {
+		frontendVersion
+	};
+}) satisfies PageServerLoad;
