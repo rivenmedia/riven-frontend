2eb66b8305ce02c7df95a20ca27033021556f634
Gaisberg <None>
2024-10-21 21:52:44 +0300

    chore: revert back client sided baseUrl configuration

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,9 @@
 import { client } from '$lib/client/services.gen';
 
+client.setConfig({
+	baseUrl: ''
+})
+
 client.interceptors.error.use((error: unknown) => {
 	if (error && typeof error == 'object' && 'detail' in error && typeof error.detail == 'string') {
 		return error.detail;
