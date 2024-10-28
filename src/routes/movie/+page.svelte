9c35fe412b75ec21214951669e555e9651d3ec16
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2024-10-28 09:29:22 +0100

    chore: remove unused html

--- src/routes/movie/+page.svelte
+++ src/routes/movie/+page.svelte
@@ -127,34 +127,5 @@
 				</Pagination.Item>
 			</Pagination.Content>
 		</Pagination.Root>
-
-		<!-- {#each data.movies.results as item}
-			<div
-				class="group relative mb-2 flex w-1/2 flex-shrink-0 flex-col gap-2 rounded-lg p-2 xl:w-1/7 xl:p-[.4rem]"
-			>
-				<div class="relative aspect-[1/1.5] w-full overflow-hidden rounded-lg">
-					<img
-						src="https://image.tmdb.org/t/p/w342{item.poster_path}"
-						alt={item.title || item.name}
-						loading="lazy"
-						class="h-full w-full object-cover object-center transition-all duration-300 ease-in-out group-hover:scale-105"
-					/>
-					<div
-						class="absolute right-0 top-1 flex items-center justify-center gap-1 rounded-l-md bg-slate-900/70 px-[5px] py-1"
-					>
-						<Star class="size-3 text-yellow-400" />
-						<span class="text-xs font-light text-white">
-							{roundOff(item.vote_average)}
-						</span>
-					</div>
-					<a
-						href="/movie/{item.id}"
-						class="absolute inset-0 hidden flex-col justify-end from-zinc-900/70 p-2 group-hover:flex group-hover:bg-gradient-to-t"
-					>
-						<ItemRequest data={item} type="movie" />
-					</a>
-				</div>
-			</div>
-		{/each} -->
 	</div>
 </div>
