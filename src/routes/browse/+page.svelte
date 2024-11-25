dffdc7b467d1626c2a8c37333103f092ecde31b5
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2024-11-25 17:36:55 +0100

    feat: ranking settings (#174)

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -105,17 +105,11 @@
 	}
 
 	function mapSelectedStates(selectedStates: Selected<string>[]) {
-		return selectedStates
-			.values()
-			.map((v) => v.value as keyof typeof states)
-			.toArray();
+		return selectedStates.map((selectedState) => selectedState.value as keyof typeof states);
 	}
 
 	function mapSelectedTypes(selectedTypes: Selected<string>[]) {
-		return selectedTypes
-			.values()
-			.map((v) => v.value as keyof typeof types)
-			.toArray();
+		return selectedTypes.map((selectedType) => selectedType.value as keyof typeof types);
 	}
 </script>
 
