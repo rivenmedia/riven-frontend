e1707355e272e476e08992c1d42596a85b02de0e
Swayam <178068815+swayam25@users.noreply.github.com>
2025-07-09 22:09:28 +0530

    Fix layout (#214)
    - Replaced `flex` with `grid` for the main layout container to ensure proper alignment and spacing.
    - Added `overflow-hidden` to the main layout to prevent overflow issues.
    - Removed `fixed` positioning from the sidebar. This allows the sidebar to function as a proper grid item.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import '../../app.css';
-	import type { LayoutProps } from './$types';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	import Sidebar from '$lib/components/sidebar.svelte';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import { ModeWatcher } from 'mode-watcher';
-	import { afterNavigate, beforeNavigate } from '$app/navigation';
 	import NProgress from 'nprogress';
-	import { Separator } from '$lib/components/ui/separator/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import Sidebar from '$lib/components/sidebar.svelte';
+	import '../../app.css';
+	import type { LayoutProps } from './$types';
 
 	let { data, children }: LayoutProps = $props();
 
@@ -29,16 +29,16 @@
 <ModeWatcher defaultMode={'dark'} />
 <Toaster richColors closeButton />
 
-<div class="bg-background flex min-h-screen">
+<div class="bg-background grid h-screen w-screen grid-cols-[auto_1fr] overflow-hidden">
 	<Sidebar user={data.user} />
-	<main class="ml-16 flex flex-1 flex-col">
-		<header class="bg-card flex h-18 items-center px-4">
+	<main class="grid grid-rows-[auto_auto_1fr] overflow-hidden">
+		<header class="bg-card flex h-18 w-full items-center px-4">
 			<div class="w-full">
 				<Input type="text" placeholder="Search..." class="h-9" />
 			</div>
 		</header>
 		<Separator class="w-full" />
-		<div class="flex-1">
+		<div class="size-full overflow-x-hidden overflow-y-auto">
 			{@render children?.()}
 		</div>
 	</main>
