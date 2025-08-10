7a26b953f8e54156ff44d9b3159972c79d2ee84d
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2025-08-11 00:21:18 +0530

    chore: change cookie prefix

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -10,5 +10,8 @@ export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true
 	},
-	plugins: [username(), sveltekitCookies(getRequestEvent)]
+	plugins: [username(), sveltekitCookies(getRequestEvent)],
+	advanced: {
+		cookiePrefix: 'riven'
+	}
 });
