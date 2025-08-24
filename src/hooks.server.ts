b269487e8b38f6cdbf08142fb3fa0e1b2ea1b303
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2025-08-24 22:44:02 +0530

    fix: add tvdb login and cookie handling

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,13 @@
 import { auth } from "$lib/server/auth";
-import { redirect, type Handle, type ServerInit } from "@sveltejs/kit";
+import { redirect, error, type Handle, type ServerInit } from "@sveltejs/kit";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 import { building } from "$app/environment";
 import { getUsersCount } from "$lib/server/functions";
 import { sequence } from "@sveltejs/kit/hooks";
 import { BACKEND_URL, BACKEND_API_KEY } from "$env/static/private";
 import { client } from "$lib/api/client.gen";
+import providers from "$lib/providers";
+import { dev } from "$app/environment";
 
 export const init: ServerInit = async () => {
     const userCount = (await getUsersCount())[0].count;
@@ -61,6 +63,34 @@ const configureClientMiddleware: Handle = async ({ event, resolve }) => {
     return resolve(event);
 };
 
+const handleTVDBCookie: Handle = async ({ event, resolve }) => {
+    const tvdbCookie = event.cookies.get("tvdb_cookie");
+
+    if (!tvdbCookie) {
+        const tvdbLogin = await providers.tvdb.POST("/login", {
+            body: {
+                apikey: "6be85335-5c4f-4d8d-b945-d3ed0eb8cdce"
+            },
+            fetch: event.fetch
+        });
+
+        if (tvdbLogin.error) {
+            error(500, "Failed to login to TVDB: " + tvdbLogin.error);
+        } else {
+            event.cookies.set("tvdb_cookie", tvdbLogin.data?.data?.token || "", {
+                path: "/",
+                httpOnly: true,
+                sameSite: "lax",
+                secure: !dev,
+                maxAge: 60 * 60 * 24 * 30 // 30 days
+            });
+            console.log("Set TVDB cookie");
+        }
+    }
+
+    return resolve(event);
+};
+
 const errorInterceptor: Handle = async ({ event, resolve }) => {
     const response = await resolve(event);
 
@@ -85,5 +115,6 @@ const errorInterceptor: Handle = async ({ event, resolve }) => {
 export const handle: Handle = sequence(
     configureClientMiddleware,
     errorInterceptor,
-    betterAuthHandler
+    betterAuthHandler,
+    handleTVDBCookie
 );
