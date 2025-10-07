d0808f12bdcd48145e579b5661e5e37e92de03ab
davidemarcoli <davide@marcoli.ch>
2025-10-07 19:27:59 +0200

    fix: hide carousel indicator on small devices

--- src/lib/components/tmdb-now-playing.svelte
+++ src/lib/components/tmdb-now-playing.svelte
@@ -146,7 +146,7 @@
         </div>
 
         <div
-            class="absolute left-1/2 z-10 flex w-full max-w-xs -translate-x-1/2 items-center justify-center gap-3 md:bottom-10 backdrop-blur-md bg-black/20 rounded-full px-4 py-2 border border-white/10">
+            class="absolute hidden lg:flex left-1/2 z-10 w-full max-w-xs -translate-x-1/2 items-center justify-center gap-3 md:bottom-10 backdrop-blur-md bg-black/20 rounded-full px-4 py-2 border border-white/10">
             <div class="text-xs font-medium whitespace-nowrap text-white/90">
                 {currentIndex + 1}/{data.length}
             </div>
