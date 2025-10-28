4771da9c24557993799562456fa6ff2735f46236
Davide Marcoli <69892203+davidemarcoli@users.noreply.github.com>
2025-10-28 20:16:38 +0100

    feat: add downloader info to dashboard (#229)
    * feat: add downloader info to dashboard
    
    * refactor: move utility functions to helpers file
    
    * refactor: optimize data fetching using Promise.all

--- src/routes/(protected)/dashboard/+page.server.ts
+++ src/routes/(protected)/dashboard/+page.server.ts
@@ -1,10 +1,14 @@
 import type { PageServerLoad } from "./$types";
-import { stats, services } from "$lib/api";
+import { stats, services, downloadUserInfo } from "$lib/api";
 import { error } from "@sveltejs/kit";
 
 export const load = (async () => {
-    const statistics = await stats();
-    const svc = await services();
+
+    const [statistics, svc, downloaderInfo] = await Promise.all([
+        stats(),
+        services(),
+        downloadUserInfo()
+    ]);
 
     if (statistics.error) {
         error(500, "Unable to fetch stats data");
@@ -16,6 +20,7 @@ export const load = (async () => {
 
     return {
         statistics: statistics.data,
-        services: svc.data
+        services: svc.data,
+        downloaderInfo: downloaderInfo.data
     };
 }) satisfies PageServerLoad;
