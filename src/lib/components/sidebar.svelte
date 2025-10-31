289c251b71795f9dfd5a68f35f12dc2b721267d8
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2025-11-01 02:26:33 +0530

    fix: update theme

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -40,7 +40,7 @@
 <aside
     class="bg-opacity-75 top-0 left-0 z-5 hidden h-screen w-14 flex-col items-center bg-transparent backdrop-blur-sm md:flex">
     <div class="flex h-18 w-full items-center justify-center">
-        <div class="text-primary-foreground flex items-center justify-center">
+        <div class="text-primary flex items-center justify-center">
             <Mountain class="size-5" />
         </div>
     </div>
