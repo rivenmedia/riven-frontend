694d5d5c0014dc818a1789467e6a6ee9303226df
Oliver Walton <olivertgwalton03@gmail.com>
2025-12-02 19:30:26 +0000

    fix: git issues

--- src/routes/(protected)/details/media/[id]/[mediaType]/+page.svelte
+++ src/routes/(protected)/details/media/[id]/[mediaType]/+page.svelte
@@ -13,11 +13,12 @@
     import ItemPause from "$lib/components/media/riven/item-pause.svelte";
     import ItemReset from "$lib/components/media/riven/item-reset.svelte";
     import ItemRetry from "$lib/components/media/riven/item-retry.svelte";
+    import ItemManualScrape from "$lib/components/media/riven/item-manual-scrape.svelte";
     import * as Dialog from "$lib/components/ui/dialog/index.js";
     import { toast } from "svelte-sonner";
-    import ItemManualScrape from "$lib/components/media/riven/item-manual-scrape.svelte";
 
     let { data }: PageProps = $props();
+    $inspect(data);
 
     const externalMetaData: Record<string, { name: string; baseUrl: string }> = {
         imdb: {
@@ -168,43 +169,6 @@
         {/if}
 
         <div class="md:px-8 lg:px-16">
-            {#if data.riven}
-                <div
-                    class="border-border mt-6 flex flex-row flex-wrap gap-2 rounded-lg border bg-white/10 px-6 py-4 shadow-lg">
-                    <ItemDelete
-                        class="bg-white/10"
-                        title={data.mediaDetails?.details.title}
-                        ids={[data.riven?.id?.toString()]}
-                        variant="destructive" />
-
-                    <ItemReset
-                        class="bg-white/10"
-                        title={data.mediaDetails?.details.title}
-                        ids={[data.riven?.id?.toString()]} />
-
-                    {#if data.riven.state !== "Completed"}
-                        <ItemPause
-                            class="bg-white/10"
-                            title={data.mediaDetails?.details.title}
-                            isPaused={data.riven.state === "Paused"}
-                            ids={[data.riven?.id?.toString()]} />
-                    {/if}
-                    <ItemRetry
-                        class="bg-white/10"
-                        title={data.mediaDetails?.details.title}
-                        ids={[data.riven?.id?.toString()]} />
-                </div>
-            {/if}
-
-            {#if data.mediaDetails?.details.id}
-                <ItemManualScrape
-                    class="bg-white/10"
-                    title={data.mediaDetails?.details.title}
-                    itemId={data.riven?.id?.toString()}
-                    externalId={data.mediaDetails?.details.id?.toString()}
-                    mediaType={data.mediaDetails?.type} />
-            {/if}
-
             <div
                 class="border-border mt-6 flex flex-row rounded-lg border bg-white/10 px-6 py-4 shadow-lg">
                 <img
@@ -226,6 +190,13 @@
                                 class="bg-white/10"
                                 title={data.mediaDetails?.details.title}
                                 ids={rivenId ? [rivenId.toString()] : []}
+                                mediaType={data.mediaDetails?.type} />
+
+                            <ItemManualScrape
+                                class="bg-white/10"
+                                title={data.mediaDetails?.details?.title}
+                                itemId={null}
+                                externalId={data.mediaDetails?.details?.id?.toString() ?? ""}
                                 mediaType={data.mediaDetails?.type} />
                         {:else if data.riven?.id != null}
                             <ItemDelete
@@ -250,6 +221,12 @@
                                 class="bg-white/10"
                                 title={data.mediaDetails?.details.title}
                                 ids={rivenId ? [rivenId.toString()] : []} />
+                            <ItemManualScrape
+                                class="bg-white/10"
+                                title={data.mediaDetails?.details?.title}
+                                itemId={rivenId?.toString() ?? null}
+                                externalId={data.mediaDetails?.details?.id?.toString() ?? ""}
+                                mediaType={data.mediaDetails?.type} />
 
                             <Dialog.Root>
                                 <Dialog.Trigger>
